dcf8ece502cf5a1a138356509496ac93ab734f64
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2021-04-29 01:40:07 -0500

    1st import for filter with arrow

--- engine/src/execution_kernels/LogicalFilter.cpp
+++ engine/src/execution_kernels/LogicalFilter.cpp
@@ -6,6 +6,29 @@
 #include "parser/expression_utils.hpp"
 #include "utilities/error.hpp"
 
+// TODO percy arrow move code
+
+namespace ral {
+namespace cpu {
+
+std::unique_ptr<ral::frame::BlazingTable> applyBooleanFilter(
+  std::shared_ptr<arrow::Table> table,
+  std::shared_ptr<arrow::ChunkedArray> boolValues){
+  //auto filteredTable = cudf::apply_boolean_mask(
+  //  table.view(),boolValues);
+  //return std::make_unique<ral::frame::BlazingTable>(std::move(
+  //  filteredTable),table.names());
+}
+
+std::vector<std::shared_ptr<arrow::ChunkedArray>> evaluate_expressions(
+    std::shared_ptr<arrow::Table> table,
+    const std::vector<std::string> & expressions) {
+  std::cout << "FILTERRRRRRRRRRRRRRRRRR ARROWWWWWWWWWWWWWWWWWWWWWWWWWWWWW!!!!!!!\n";
+}
+
+} // namespace cpu
+} // namespace ral
+
 namespace ral {
 namespace processor {
 namespace {
@@ -46,11 +69,20 @@ std::unique_ptr<ral::frame::BlazingTable> process_filter(
 		conditional_expression = get_named_expression(query_part, "filters");
 	}
 
-  std::vector<std::unique_ptr<ral::frame::BlazingColumn>> evaluated_table = evaluate_expressions(table_view.view(), {conditional_expression});
+  if (table_view.is_arrow()) {
+    std::vector<std::shared_ptr<arrow::ChunkedArray>> evaluated_table = ral::cpu::evaluate_expressions(table_view.arrow_table(), {conditional_expression});
 
-  RAL_EXPECTS(evaluated_table.size() == 1 && evaluated_table[0]->view().type().id() == cudf::type_id::BOOL8, "Expression does not evaluate to a boolean mask");
+    // TODO percy arrow
+    //RAL_EXPECTS(evaluated_table.size() == 1 && evaluated_table[0]->view().type().id() == cudf::type_id::BOOL8, "Expression does not evaluate to a boolean mask");
 
-  return applyBooleanFilter(table_view, evaluated_table[0]->view());
+    return ral::cpu::applyBooleanFilter(table_view.arrow_table(), evaluated_table[0]);
+  } else {
+    std::vector<std::unique_ptr<ral::frame::BlazingColumn>> evaluated_table = evaluate_expressions(table_view.view(), {conditional_expression});
+
+    RAL_EXPECTS(evaluated_table.size() == 1 && evaluated_table[0]->view().type().id() == cudf::type_id::BOOL8, "Expression does not evaluate to a boolean mask");
+
+    return applyBooleanFilter(table_view, evaluated_table[0]->view());
+  }
 }
 
 
