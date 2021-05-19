228b603071456f6e555a765e43e1d45fdb2ad3ab
Romulo Auccapuclla <romulo@blazingdb.com>
2021-05-18 20:11:01 -0500

    add all option for dependencies script

--- dependencies.sh
+++ dependencies.sh
@@ -7,9 +7,10 @@ set -e
 NUMARGS=$#
 ARGS=$*
 
-VALIDARGS="rapids cuda -h nightly e2e mysql postgres snowflake"
+VALIDARGS="rapids cuda -h nightly all e2e mysql postgres snowflake"
 
 HELP="$0 [-h] rapids=[rapids version] cuda=[cuda version]
+  all         - Will install e2e mysql postgres and snowflake dependencies
   e2e         - Install e2e basic dependencies for e2e tests
   -h          - Print help text
   mysql       - Install mysql dependencies for e2e tests
@@ -60,6 +61,8 @@ if (( ${NUMARGS} != 0 )); then
           exit 1
       fi
     done
+else
+    echo "USAGE: ${HELP}"
 fi
 
 if hasArg nightly; then
@@ -67,7 +70,7 @@ if hasArg nightly; then
 fi
 
 if [ "$RAPIDS_VERSION" ] && [ "$CUDA_VERSION" ]; then
-  echo -e "${GREEN}Installing dependencies${ENDCOLOR}"
+  echo -e "${GREEN}Installing build dependencies${ENDCOLOR}"
   conda install --yes -c conda-forge spdlog'>=1.7.0,<2.0.0a0' google-cloud-cpp=1.25 ninja mysql-connector-cpp=8.0.23 libpq=13 nlohmann_json=3.9.1 unixodbc=2.3.9
   # NOTE cython must be the same of cudf (for 0.11 and 0.12 cython is >=0.29,<0.30)
   conda install --yes -c conda-forge cmake=3.18 gtest==1.10.0=h0efe328_4 gmock cppzmq cython=0.29 openjdk=8.0 maven jpype1 netifaces pyhive pytest tqdm ipywidgets boost-cpp=1.76.0
@@ -76,21 +79,24 @@ if [ "$RAPIDS_VERSION" ] && [ "$CUDA_VERSION" ]; then
   conda install --yes -c rapidsai$CHANNEL -c nvidia -c conda-forge -c defaults dask-cuda=$RAPIDS_VERSION dask-cudf=$RAPIDS_VERSION cudf=$RAPIDS_VERSION ucx-py=$RAPIDS_VERSION ucx-proc=*=gpu cudatoolkit=$CUDA_VERSION
 fi
 
-if hasArg e2e; then
+if hasArg all || hasArg e2e; then
   echo -e "${GREEN}Install E2E test dependencies${ENDCOLOR}"
   conda install --yes -c conda-forge openjdk=8.0 maven pyspark=3.0.0 pytest
   pip install pydrill openpyxl pymysql gitpython pynvml gspread oauth2client docker
 fi
 
-if hasArg mysql; then
+if hasArg all || hasArg mysql; then
+  echo -e "${GREEN}Install mysql test dependencies${ENDCOLOR}"
   conda install --yes -c conda-forge mysql-connector-python
 fi
 
-if hasArg postgres; then
+if hasArg all || hasArg postgres; then
+  echo -e "${GREEN}Install postgres test dependencies${ENDCOLOR}"
   conda install --yes -c conda-forge psycopg2
 fi
 
-if hasArg snowflake; then
+if hasArg all || hasArg snowflake; then
+  echo -e "${GREEN}Install snowflake test dependencies${ENDCOLOR}"
   pip install snowflake-connector-python==2.4.2
 fi
 if [ $? -eq 0 ]; then
