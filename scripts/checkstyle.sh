b5733b909bf2411761803ee555dfe4b3f4b7627b
Romulo Auccapuclla <romulo@blazingdb.com>
2021-05-25 11:43:58 -0500

    atomic checkstyle

--- scripts/checkstyle.sh
+++ scripts/checkstyle.sh
@@ -31,20 +31,29 @@ function getFiles() {
         return "$files"
     fi
 }
-function StyleBlack {
+function CheckStyleBlack {
     BLACK=$(black --check "$1" --diff)
-    BLACK_RETVAL=$?
 }
 
-function StyleFlake8 {
+function ApplyStyleBlack {
+    BLACK=$(black --check "$1" --diff)
+}
+
+function CheckStyleFlake8 {
     FLAKE8=$(black --check "$1" --diff)
-    FLAKE8_RETVAL=$?
 }
 
-function StyleClangFormat {
-    CLANG_FORMAT=$(clang-format "$1" > "$2")
-    CLANG_FORMAT_RETVAL=$?
+function ApplyStyleFlake8 {
+    FLAKE8=$(black --check "$1" --diff)
+}
+
+function CheckStyleClangFormat {
+    clang-format "$1" > "$2"
     CLANG_FORMAT_DIFF=$(diff "$1" "$2")
+}
+
+function ApplyStyleClangFormat {
+    clang-format -i "$1" -fallback-style=none
 }
 
 # Install dependencies
@@ -65,8 +74,32 @@ if hasArg --check; then
                 echo -e "Flake8 check Style >>>> ${FLAKE8}"
             elif [[ $extension =~ (cu|cuh|h|hpp|cpp|inl) ]]; then
                 StyleClangFormat "$file" "$tmpDir/$filename"
-                echo -e "DIFF >> ${CLANG_FORMAT_DIFF}"
+                echo -e ">>>>>>>>BEGIN DIFF [$file]>>>>>>>"
+                echo -e "${CLANG_FORMAT_DIFF}"
+                echo -e "<<<<<<<<END DIFF [$file]<<<<<<<"
             fi
         fi
     done
+    echo -e "Cleanup temporal files"
+    rm -rf "$tmpDir"
+    echo -e "You have 2 options to fix checkstyle observations"
+    echo -e "1. Apply checkstyle executing ./scripts/checkstyle.sh [file] --fix a the commit your changes"
+    echo -e "2. Manually update the file to conform the checkstyle rules"
+fi
+if hasArg --fix; then
+    for file in "${files[@]}";
+    do
+        if test -f "$file"; then
+            #check extension
+            filename=$(basename -- "$file")
+            extension="${filename##*.}"
+            if [[ $extension =~ (cu|cuh|h|hpp|cpp|inl) ]]; then
+                ApplyStyleClangFormat "$file"
+                echo -e "Applying clang-format to $file"
+            else
+                echo -e "File $file skipped!!!"
+            fi
+        fi
+    done
+    echo -e "Apply clang-format complete!!!"
 fi
