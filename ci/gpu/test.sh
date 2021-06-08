6165cee8da27ff4b9009fc505f78e8885a350da9
Fernando Rodriguez <diegodfrf@gmail.com>
2021-06-08 11:08:40 -0500

    update test.sh

--- ci/gpu/test.sh
+++ ci/gpu/test.sh
@@ -1,10 +1,84 @@
 #!/bin/bash
 
-NUMARGS=$#
-ARGS=$*
+OPTIONS=t:c:vkh
+LONGOPTS=io,libengine,algebra,pyblazing,e2e,tests:,config-file:,verbose,skipe2e,help
 
-# Abort script on first error
-set -e
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+
+eval set -- "$PARSED"
+
+io=No
+libengine=No
+algebra=No
+pyblazing=No
+e2e=No
+tests=""
+configfile=""
+verbose=No
+skipe2e=No
+printhelp=No
+testAll=true
+
+while true; do
+    case "$1" in
+        --io)
+            io=Yes
+            testAll=false
+            shift
+            ;;
+        --libengine)
+            libengine=Yes
+            testAll=false
+            shift
+            ;;
+        --algebra)
+            algebra=Yes
+            testAll=false
+            shift
+            ;;
+        --pyblazing)
+            pyblazing=Yes
+            testAll=false
+            shift
+            ;;
+        --e2e)
+            e2e=Yes
+            testAll=false
+            shift
+            ;;
+        -t|--tests)
+            tests="$2"
+            shift 2
+            ;;
+        -c|--config-file)
+            configfile="$2"
+            shift 2
+            ;;
+        -v|--verbose)
+            verbose=Yes
+            shift
+            ;;
+        -k|--skipe2e)
+            skipe2e=Yes
+            shift
+            ;;
+        -h|--help)
+            printhelp=Yes
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
 
 if [ -n $CONDA_PREFIX ]; then
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib:$CONDA_PREFIX/lib64
@@ -15,29 +89,30 @@ function logger() {
   echo -e "\n>>>> $@\n"
 }
 
-# NOTE: ensure all dir changes are relative to the location of this
-# script, and that this script resides in the repo dir!
-REPODIR=$(cd $(dirname $0); pwd)
+HELP="Voltron testing
+Tool to run Unit Test and End-to-End tests.
 
-# TODO william kharoly felipe we should try to enable and use this param in the future (compare result from spreadsheet): add -c
-VALIDARGS="io libengine algebra pyblazing e2e -t -v -h tests"
-HELP="$0 [-v] [-h] [-t] [-c] [e2e_test=\"test1,test2,...,testn\"]
-   io           - test the IO C++ code only
-   libengine    - test the engine C++ code only
-   algebra      - test the algebra package
-   pyblazing    - test the pyblazing interface
-   e2e          - test the end to end tests
-   -t           - skip end to end tests (force not run 'e2e' tests)
-   -v           - verbose test mode
-   -h           - print this text
-   tests=       - Optional argument to use after 'e2e' run specific e2e
-                  test groups.
-                  The comma separated values are the e2e tests to run, where
-                  each value is the python filename of the test located in
-                  blazingsql/tests/BlazingSQLTest/EndToEndTests/
-                  (e.g. 'castTest, groupByTest' or 'literalTest' for single test).
-                  Empty means will run all the e2e tests)
-   default action (no args) is to test all code and packages
+Usage:
+   test.sh                          Default action (no args) is to test all code and packages
+   test.sh [options]
+
+Options:
+   --io                             Test the IO C++ code only
+   --libengine                      Test the engine C++ code only
+   --algebra                        Test the algebra package
+   --pyblazing                      Test the pyblazing interface
+   --e2e                            Test the end to end tests
+   -k, --skipe2e                    Skip end to end tests (force not run 'e2e' tests)
+   -v, --verbose                    Verbose test mode
+   -h, --help                       Print help
+   -t=<name>, --tests=<name>        Optional argument to use after 'e2e' run specific e2e test groups.
+                                    The comma separated values are the e2e tests to run, where
+                                    each value is the python filename of the test located in
+                                    blazingsql/tests/BlazingSQLTest/EndToEndTests/
+                                    (e.g. 'castTest, groupByTest' or 'literalTest' for single test).
+                                    Empty means will run all the e2e tests)
+   -c <file>, --config-file <file>  Set configuration file for E2E tests, the file must be located
+                                    in blazingsql/tests/BlazingSQLTest/Runner/
 "
 
 # TODO william kharoly felipe we should try to enable and use this param in the future (compare result from spreadsheet)
@@ -47,41 +122,11 @@ HELP="$0 [-v] [-h] [-t] [-c] [e2e_test=\"test1,test2,...,testn\"]
 #                   is usually pointing to the CONDA_PREFIX dir)
 
 # Set defaults for vars modified by flags to this script
-VERBOSE=""
-QUIET="--quiet"
-TESTS="ON"
-GSPREAD_CACHE="false"
-TARGET_E2E_TEST_GROUPS=""
+#GSPREAD_CACHE="false"
 
-function hasArg {
-    (( ${NUMARGS} != 0 )) && (echo " ${ARGS} " | grep -q " $1 ")
-}
-
-function testAll {
-    ((${NUMARGS} == 0 )) || !(echo " ${ARGS} " | grep -q " [^-]\+ ")
-}
-
-if hasArg -h; then
-    echo "${HELP}"
-    exit 0
-fi
-
-# Check for valid usage
-if (( ${NUMARGS} != 0 )); then
-    for a in ${ARGS}; do
-    if [[ $a == *"="* ]]; then
-        TARGET_E2E_TEST_GROUPS=${a#"tests="}
-        if [ $TARGET_E2E_TEST_GROUPS == $a ] ; then
-            echo "Invalid option: ${a}"
-            exit 1
-        fi
-        continue
-    fi
-    if ! (echo " ${VALIDARGS} " | grep -q " ${a} "); then
-        echo "Invalid option: ${a}"
-        exit 1
-    fi
-    done
+if [ "$printhelp" == "Yes" ]; then
+  echo "${HELP}"
+  exit 0
 fi
 
 # NOTE if WORKSPACE is not defined we assume the user is in the blazingsql project root folder
@@ -91,58 +136,60 @@ if [ -z $WORKSPACE ] ; then
     WORKSPACE=$PWD
 fi
 
-# Process flags
-if hasArg -v; then
-    VERBOSE=1
-    QUIET=""
-fi
-if hasArg -t; then
-    TESTS="OFF"
-fi
-
 # TODO william kharoly felipe we should try to enable and use this param in the future (compare result from spreadsheet)
 #if hasArg -c; then
 #    GSPREAD_CACHE="true"
 #fi
 
 ################################################################################
-
-if testAll || hasArg io; then
+if $testAll || [ "$io" == "Yes" ]; then
     logger "Running IO Unit tests..."
     cd ${WORKSPACE}/io/build
     SECONDS=0
-    ctest --verbose
+    if [ "$verbose" == "Yes" ]; then
+      ctest --verbose
+    else
+      ctest
+    fi
     duration=$SECONDS
     echo "Total time for IO Unit tests: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
 fi
 
-if testAll || hasArg libengine; then
+if $testAll || [ "$libengine" == "Yes" ]; then
     logger "Running Engine Unit tests..."
     cd ${WORKSPACE}/engine/build
     SECONDS=0
-    ctest --verbose
+    if [ "$verbose" == "Yes" ]; then
+      ctest --verbose
+    else
+      ctest
+    fi
     duration=$SECONDS
     echo "Total time for Engine Unit tests: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
 fi
 
-if testAll || hasArg algebra; then
+if $testAll || [ "$algebra" == "Yes" ]; then
     # TODO mario
     echo "TODO"
 fi
 
-if testAll || hasArg pyblazing; then
+if $testAll || [ "$pyblazing" == "Yes" ]; then
     logger "Running Pyblazing Unit tests..."
     SECONDS=0
     cd ${WORKSPACE}/pyblazing/tests
-    pytest
+    if [ "$verbose" == "Yes" ]; then
+      pytest --verbose
+    else
+      pytest
+    fi
     duration=$SECONDS
     echo "Total time for Pyblazing Unit tests: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
 fi
 
-if [ "$TESTS" == "OFF" ]; then
+if [ "$skipe2e" == "Yes" ]; then
     logger "Skipping end to end tests..."
 else
-    if testAll || hasArg e2e; then
+    if $testAll || [ "$e2e" == "Yes" ]; then
         if [ -z $BLAZINGSQL_E2E_DATA_DIRECTORY ] || [ -z $BLAZINGSQL_E2E_FILE_RESULT_DIRECTORY ]; then
             if [ -d $CONDA_PREFIX/blazingsql-testing-files/data/ ]; then
                 logger "Using $CONDA_PREFIX/blazingsql-testing-files folder for end to end tests..."
@@ -193,43 +240,20 @@ else
         # TODO william kharoly felipe we should try to enable and use this param in the future (compare result from spreadsheet)
         #export BLAZINGSQL_E2E_GSPREAD_CACHE=$GSPREAD_CACHE
 
-        export BLAZINGSQL_E2E_TARGET_TEST_GROUPS=$TARGET_E2E_TEST_GROUPS
+        export BLAZINGSQL_E2E_TARGET_TEST_GROUPS=$tests
 
-        for include_nulls in "false" "true"; do
-            # If we are running on a GPUCI environment then force to set nrals to 1
-            if [ "$BLAZINGSQL_E2E_IN_GPUCI_ENV" == "true" ] ; then
-                logger "Running end to end tests SINGLE NODE (nrals=1), including nulls: $include_nulls ..."
-                export BLAZINGSQL_E2E_N_RALS=1
-                export BLAZINGSQL_E2E_TEST_WITH_NULLS=$include_nulls
-            fi
+        cd ${WORKSPACE}/tests/BlazingSQLTest/
 
-            cd ${WORKSPACE}/tests/BlazingSQLTest/
-            SECONDS=0
-
+        if [ "$configfile" != "" ]; then
+          echo "Loading config file: ${configfile}"
+          python -m EndToEndTests.mainE2ETests --config-file $configfile
+        else
+          if [ "$BLAZINGSQL_E2E_IN_GPUCI_ENV" == "true" ] ; then
+            python -m EndToEndTests.mainE2ETests --config-file config_singlenode.yaml
+            python -m EndToEndTests.mainE2ETests --config-file config_multinode.yaml
+          else
             python -m EndToEndTests.mainE2ETests --config-file config.yaml
-
-            duration=$SECONDS
-            logger "Total time for end to end tests: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
-
-            # If we are running on a GPUCI environment then print final status for nrals=1
-            if [ "$BLAZINGSQL_E2E_IN_GPUCI_ENV" == "true" ] ; then
-                logger "End to end tests SINGLE NODE (nrals=1), including nulls: $include_nulls ... DONE!"
-            fi
-
-            # If we are running on a GPUCI environment then run again the e2e but with nrals=2
-            if [ "$BLAZINGSQL_E2E_IN_GPUCI_ENV" == "true" ] ; then
-                logger "Running end to end tests DISTRIBUTED (nrals=2), including nulls: $include_nulls ..."
-                export BLAZINGSQL_E2E_N_RALS=2
-                export BLAZINGSQL_E2E_TEST_WITH_NULLS=$include_nulls
-                cd ${WORKSPACE}/tests/BlazingSQLTest/
-                SECONDS=0
-
-                python -m EndToEndTests.mainE2ETests --config-file config.yaml
-
-                duration=$SECONDS
-                logger "Total time for end to end tests: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
-                logger "End to end tests DISTRIBUTED (nrals=2), including nulls: $include_nulls ... DONE!"
-            fi
-        done
+          fi
+        fi
     fi
 fi
