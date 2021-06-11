d389a4aa6a5e90717b07e20c24e61c1dbdb30db9
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2021-06-11 18:14:39 -0500

    less vars

--- tests/BlazingSQLTest/Utils/validatorYaml.py
+++ tests/BlazingSQLTest/Utils/validatorYaml.py
@@ -7,16 +7,12 @@ def validate_config(filename):
     file_schema = 'schema_config.yaml'
     schema = yamale.make_schema(cwd + '/../Configuration/' + file_schema)
 
-    filename_path = filename # arg[2]
-    parent_dir = os.path.dirname(filename)
-    file_schema_path = parent_dir + "/" + file_schema
-
     # try the full path: don't force the user to use Configuration folder
-    if not os.path.isfile(filename_path):
-      filename_path = cwd + '/../Configuration/' + filename_path
+    if not os.path.isfile(filename):
+      filename = cwd + '/../Configuration/' + filename
 
     try:
-        data = yamale.make_data(filename_path)
+        data = yamale.make_data(filename)
         yamale.validate(schema, data)
         print('Validation success for file "' + filename + '" with schema "' + file_schema + '"')
     except ValueError as e:
