987184ee0be2593c0ccc976fce10e44c5bc2f36a
Fernando Rodriguez <diegodfrf@gmail.com>
2021-06-18 08:32:26 -0500

    add comment var unused

--- engine/src/compute/arrow/detail/io.cpp
+++ engine/src/compute/arrow/detail/io.cpp
@@ -74,9 +74,8 @@ void getCsvReaderOptions(const std::map<std::string, std::string> &args,
 std::unique_ptr<ral::frame::BlazingTable> read_parquet_file(
     std::shared_ptr<arrow::io::RandomAccessFile> file,
     std::vector<int> column_indices,
-    std::vector<std::string> col_names,
+    std::vector<std::string> /*col_names*/,
     std::vector<cudf::size_type> row_groups)
-
 {
   // Open Parquet file reader
   arrow::MemoryPool* pool = arrow::default_memory_pool();
